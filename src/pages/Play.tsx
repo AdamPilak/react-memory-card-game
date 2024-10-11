ef71de7525987cbb02b9351d500e99a98eb536ae
AdamPilak <pilak.adam@gmail.com>
2024-10-11 10:56:38 +0200

    home page

---
+++ src/pages/Play.tsx
@@ -0,0 +1,5 @@
+const Play = () => {
+  return <section className="play"></section>;
+};
+
+export default Play;
--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "./layouts/Layout";
 import Home from "./pages/Home";
+import Play from "./pages/Play";
 
 const router = createBrowserRouter([
   {
@@ -8,6 +9,14 @@ const router = createBrowserRouter([
     element: (
       <Layout>
         <Home />
+      </Layout>
+    ),
+  },
+  {
+    path: "/play",
+    element: (
+      <Layout>
+        <Play />
       </Layout>
     ),
   },
